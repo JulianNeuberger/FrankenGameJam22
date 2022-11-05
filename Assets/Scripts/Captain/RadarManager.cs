d8cef85df63de4687344e11cfeb4c848eed96332
Beztix <sebastian.luetzow@gmail.com>
2022-11-05 14:46:42 +0100

    add 2D radar and update radarDiverMarker

--- Assets/Scripts/Captain/RadarManager.cs
+++ Assets/Scripts/Captain/RadarManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class RadarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float diverPositionMaxX;
+    public float diverPositionMaxZ;
+
+    public float radarMaxX;
+    public float radarMaxY;
+
+    public GameObject diverRadarMarker;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //=== update diverRadarMarker position ===
+        var oldDiverRadarMarkerPosition = diverRadarMarker.transform.position;
+
+        //default
+        float diverRadarX = 0;
+        float diverRadarY = 0;
+
+        //map diverPosition in 3D to diverRadarPosition in 2D (mapping 3D z-axis to 2D y-axis)
+        if (NetworkSyncer.Get())
+        {
+            Vector3 diverPosition = NetworkSyncer.Get().diverPosition.Value;
+
+            diverRadarX = diverPosition.x / diverPositionMaxX * radarMaxX;
+            diverRadarY = diverPosition.z / diverPositionMaxZ * radarMaxY;
+        }
+
+        diverRadarMarker.transform.position = new Vector3(diverRadarX, diverRadarY, oldDiverRadarMarkerPosition.z);
     }
 }
